d3286d8c402e1ba79b0dae0fa9491e21b1ddfe61
Matheus Gomes da silva <aquersaber@gmail.com>
2024-02-26 22:27:34 -0300

    initial commit

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Notes
+{
+
+
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string nome = "";
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void onNameClicked(object sender, EventArgs e)
+        {
+
+            nome = NameEntry.Text;
+            GreetingLabel.Text = $"Hello, {nome}";
+
+            SemanticScreenReader.Announce(GreetingLabel.Text);
+        }
+
+    }
+
+}
